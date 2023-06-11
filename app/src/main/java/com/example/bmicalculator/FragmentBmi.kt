3de8e822469fe455328cbcb21c34df2ece3f4dfb
Adrian-Silver <adrianallangitau@gmail.com>
2023-06-11 21:39:10 +0300

    metric spinner added

--- app/src/main/java/com/example/bmicalculator/FragmentBmi.kt
+++ app/src/main/java/com/example/bmicalculator/FragmentBmi.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.bmicalculator.databinding.FragmentBmiBinding
+import kotlinx.coroutines.NonDisposableHandle.parent
 import java.math.RoundingMode
 import java.text.DecimalFormat
 
@@ -29,6 +33,28 @@ class FragmentBmi : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val measurementSystems = arrayOf("Metric System", "US Customary System")
+
+        val spinnerAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, measurementSystems)
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        binding.metricSpinner.adapter = spinnerAdapter
+
+//        binding.metricSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+//                val selectedItem = parent.getItemAtPosition(position).toString()
+//
+//                val meszSystem = when (selectedItem) {
+//                    "Metric System" ->
+//                }
+//
+//            }
+//
+//            override fun onNothingSelected(parent: AdapterView<*>?) {
+//                TODO("Not yet implemented")
+//            }
+//
+//        }
 
         binding.calculateBtn.setOnClickListener {
             calculateBmi()
@@ -46,7 +72,13 @@ class FragmentBmi : Fragment() {
         val height = binding.heightEt.text.toString().toDouble()
         val weight = binding.weightEt.text.toString().toDouble()
 
-        val result = bmi(height, weight)
+        val measurementSystem = binding.metricSpinner.selectedItem.toString()
+
+        val result = if (measurementSystem == "US Customary System") {
+            bmi(height, weight) * 703
+        } else {
+            bmi(height, weight)
+        }
 
         // Rounding up the result to 2 d.p
         val df = DecimalFormat("#.##")
@@ -54,15 +86,31 @@ class FragmentBmi : Fragment() {
 
         val roundedResult = df.format((result))
 
-        binding.outputTv.text = "Your BMI is: $roundedResult"
+//        binding.outputTv.text = "Your BMI is: $roundedResult"
+        binding.numResultTv.text = "$roundedResult"
     }
+
+//    private fun metricSystem() {
+//
+//    }
 
     // TODO
     // Have height options for metres and cm and Inches and Feet
     // Have weight options for kgs and Pounds
     // Conversion operations
 
+
+    /* Conversions half-done - Issue:
+    * Height is converted to inches - what if I want in feet and inches?
+    * Need to change height to match conversion
+    * use sharedPreferences to retain conversion system
+    * Based on conversion system, let it reflect on hint
+    */
+
+
     // Describe BMI result with different colours(blue, green, yellow, red)
+    // Create a status bar showing the result and color
+
 
 
 }
